9f02ebf848c71e3be8afba8d00d8e631f52812fc
hamza bellamine <hamza.bellamine@gmail.com>
2021-05-13 13:03:47 +0200

    changes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const app = require("express")();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const config = {
+    apiKey: "AIzaSyB91ShPu9QyJ7uVcRnbmdCtrfk7HC-Zd0E",
+    authDomain: "qwizards-5b134.firebaseapp.com",
+    databaseURL: "https://qwizards-5b134.firebaseio.com",
+    projectId: "qwizards-5b134",
+    storageBucket: "qwizards-5b134.appspot.com",
+    messagingSenderId: "239116512188",
+    appId: "1:239116512188:web:502016d7383894d2c259b9",
+    measurementId: "G-GKTG27JRBF"
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+app.get("/getbooks", (req, res) => {
+  db.collection("bookslist")
+    .get()
+    .then((data) => {
+      let books = [];
+      data.forEach((doc) => {
+        books.push(doc.data());
+      });
+      return res.json(books);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.api = functions.https.onRequest(app);
